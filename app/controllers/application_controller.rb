a476e9e9f46195ce6608043b77b54b8e4bd82aea
nicolefederici <nicolefederici@yahoo.com>
2016-11-10 23:22:05 -0500

    built out application controller; actually, forgot to save before previous commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions 
-    set :session_secret, "fuck_sinatra"
+    set :session_secret, "zounds"
   end
 
    helpers do
